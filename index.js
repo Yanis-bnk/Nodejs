217d60253c2d6c4cb85eec1030c8beea3a95ac02
yanis <benikenyanis1@gmail.com>
2025-02-28 19:06:09 +0100

    Pre

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const app=express()
 app.get("/helloword",(req,res)=>{
     res.send('hello yanis beniken')
 })
+app.get("/HI",(req,res)=>{
+    res.send('tu es dans hi')
+})
+app.get("/test",(req,res)=>{
+    res.send('tu es dans test')
+})
+app.post("/postt",(req,res)=>{
+    res.send('tu es dans post')
+})
+app.get("/get", (req, res) => {
+    let number = "";
+    for (let i = 0; i <= 100; i++) {  
+        number += i + "-";
+    }
+    res.send(`Les nombres sont ${number}`); 
+});
+app.listen(3000,()=>{
+    console.log('tu travaille sur le port 3000')
+})
+    app.get("/Api/:number1/:number2",(req,res)=>{
+        res.send('tu es dans post')
+    })
